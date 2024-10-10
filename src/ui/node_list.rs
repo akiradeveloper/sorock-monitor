ce2253ebde764be926a2db15ba39bb3117078485
Akira Hayakawa <ruby.wktk@gmail.com>
2024-10-10 14:25:03 +0900

    upd
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/ui/node_list.rs
+++ src/ui/node_list.rs
@@ -28,7 +28,26 @@ struct LogStripe {
     last_to_max: u16,
 }
 impl LogStripe {
-    pub fn from(node: &Node) -> Self {}
+    pub fn from(node: &Node) -> Self {
+        let len = node.min_max.max_index - node.min_max.min_index;
+
+        let min_to_head = (node.head_index - node.min_max.min_index) as f64 / len as f64;
+        let min_to_snap = (node.snapshot_index - node.min_max.min_index) as f64 / len as f64;
+        let min_to_app = (node.app_index - node.min_max.min_index) as f64 / len as f64;
+        let min_to_commit = (node.commit_index - node.min_max.min_index) as f64 / len as f64;
+        let min_to_last = (node.last_index - node.min_max.min_index) as f64 / len as f64;
+
+        let k = 10000.0;
+
+        Self {
+            min_to_head: (min_to_head * k) as u16,
+            head_to_snap: ((min_to_snap - min_to_head) * k) as u16,
+            snap_to_app: ((min_to_app - min_to_snap) * k) as u16,
+            app_to_commit: ((min_to_commit - min_to_app) * k) as u16,
+            commit_to_last: ((min_to_last - min_to_commit) * k) as u16,
+            last_to_max: ((1.0 - min_to_last) * k) as u16,
+        }
+    }
 }
 
 pub struct Node {
@@ -61,6 +80,7 @@ impl Widget for Node {
                     Constraint::Fill(stripe.snap_to_app),
                     Constraint::Fill(stripe.app_to_commit),
                     Constraint::Fill(stripe.commit_to_last),
+                    Constraint::Fill(stripe.last_to_max),
                 ]
                 .as_ref(),
             )
